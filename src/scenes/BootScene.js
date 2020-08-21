6d7a7fe6d2d335ed6730b4105e1189821a70acf7
modupe <adewalebose15@gmail.com>
2020-08-21 15:29:26 +0100

    add preload and titlescreen

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -6,8 +6,10 @@ export default class BootScene extends Phaser.Scene {
   }
  
   preload () {
+    this.load.image('logo', 'assets/adventure.png');
   }
  
   create () {
+    this.scene.start('Preloader');
   }
 };
